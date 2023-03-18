2c2fde91146cf97e7f7197e88a42e14816928bd7
Daniel Rahme <daniel.rahme.dr@gmail.com>
2023-03-18 20:07:05 +0100

    Create TreeSitter lua file and GLSL highlight

---
+++ my-configs/config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "cpp",
+      "c",
+      "python",
+      "glsl",
+      "bash",
+      "help",
+      "html",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "regex",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+  },
+}

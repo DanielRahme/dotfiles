43d08825e8271352313623ea819c55e4a419406f
Daniel Rahme <daniel.rahme.dr@gmail.com>
2023-03-18 20:06:02 +0100

    Move plugins to plugins.lua file

---
+++ my-configs/config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,16 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+return {
+
+  { "ThePrimeagen/harpoon" },
+  { "tpope/vim-fugitive" },
+  { "mbbill/undotree" },
+  { "kylechui/nvim-surround" },
+  { "kalvinpearce/ShaderHighlight" },
+
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+}

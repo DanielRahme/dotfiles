3d020b0e29bad5c6b6f08106741dc22054a88fd1
Daniel Rahme <daniel.rahme.dr@gmail.com>
2023-03-28 23:10:39 +0200

    Change color to visible line when splitting windows

--- my-configs/config/nvim/lua/plugins/plugins.lua
+++ my-configs/config/nvim/lua/plugins/plugins.lua
@@ -6,11 +6,18 @@ return {
   { "mbbill/undotree" },
   { "kylechui/nvim-surround" },
   { "kalvinpearce/ShaderHighlight" },
-
   {
     "simrat39/symbols-outline.nvim",
     cmd = "SymbolsOutline",
     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
     config = true,
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      on_colors = function(colors)
+        colors.border = "#565f89"
+      end,
+    },
   },
 }
